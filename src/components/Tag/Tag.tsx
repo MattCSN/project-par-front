00b2911ee075c1299a5f389fe8b99e070dd71552
Matt <matthieu.coisnon@decathlon.com>
2024-08-12 10:30:00 +0200

    feat(home-page): create Tag components

---
+++ src/components/Tag/Tag.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./Tag.css";
+
+interface TagProps {
+    text: string;
+    type: string;
+}
+
+const Tag: React.FC<TagProps> = ({ text, type }) => {
+    return (
+        <span className={`tag ${type}`}>
+            {text}
+        </span>
+    );
+};
+
+export default Tag;
