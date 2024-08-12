1856fd59c639703e71a39e87704b274cf0643072
Matt <matthieu.coisnon@decathlon.com>
2024-08-12 10:32:58 +0200

    feat(home-page): create HomePage

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,67 @@
+import {useState} from 'react';
+import {useQuery} from 'react-query';
+import {getCourseDetails, searchCourseDetails} from '../../services/courseService';
+import CourseCardGrid from '../../components/CourseCardGrid/CourseCardGrid.tsx';
+import Paginator from '../../components/Paginator/Paginator.tsx';
+import Header from '../../components/Header/Header.tsx';
+import './HomePage.css';
+
+const HomePage = () => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [searchResults, setSearchResults] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const {data: coursesDTO, error, isLoading} = useQuery(
+        ['courseDetails', currentPage],
+        () => getCourseDetails(currentPage, 12),
+        {enabled: !searchQuery}
+    );
+
+    const fetchCourseDetails = async (query: string, page: number) => {
+        try {
+            const data = await searchCourseDetails(query, page, 12);
+            setSearchResults(data);
+        } catch {
+            setSearchResults([]);
+        }
+    };
+
+    const handleSearch = async (query: string) => {
+        setCurrentPage(1);
+        setSearchQuery(query);
+        await fetchCourseDetails(query, 1);
+    };
+
+    const handlePageChange = async (newPage: number) => {
+        setCurrentPage(newPage);
+        if (searchQuery) {
+            await fetchCourseDetails(searchQuery, newPage);
+        }
+    };
+
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>Error: {(error as Error).message}</div>;
+
+    const showPaginator = searchQuery
+        ? currentPage == 1 ? searchResults.length == 12 : searchResults
+        : currentPage == 1 ? coursesDTO.length == 12 : coursesDTO
+
+    return (
+        <div className="global-container">
+            <Header onSearch={handleSearch}/>
+            <div className="home-page-content">
+                <CourseCardGrid courses={searchQuery ? searchResults : coursesDTO}/>
+            </div>
+            {showPaginator && (
+                <Paginator
+                    currentPage={currentPage}
+                    itemsCount={searchQuery ? searchResults.length : coursesDTO.length}
+                    onNextPage={() => handlePageChange(currentPage + 1)}
+                    onPreviousPage={() => handlePageChange(Math.max(currentPage - 1, 1))}
+                />
+            )}
+        </div>
+    );
+};
+
+export default HomePage;
