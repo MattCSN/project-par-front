4fdcb88f8728a4e08819e26c4e91cd43d96c41a2
Matt <matthieu.coisnon@decathlon.com>
2024-08-12 10:29:38 +0200

    feat(home-page): create Buttons components

---
+++ src/components/Buttons/PrimaryButton/PrimaryButton.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Button from "../Button/Button.tsx";
+
+import "./PrimaryButton.css";
+
+interface PrimaryButtonProps {
+    text: string;
+    onClick: () => void;
+    disabled?: boolean;
+}
+
+const PrimaryButton: React.FC<PrimaryButtonProps> = ({text, onClick, disabled = false}) => {
+    return (
+        <Button text={text} className="primary-button" onClick={onClick} disabled={disabled}/>
+    );
+}
+
+export default PrimaryButton;
