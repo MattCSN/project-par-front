e05c06bdf0fd31359fd7c7d18b334488f67c2956
Matt <matthieu.coisnon@decathlon.com>
2024-08-12 10:29:38 +0200

    feat(home-page): create Buttons components

---
+++ src/components/Buttons/Button/Button.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./Button.css";
+
+interface ButtonProps {
+    text: string;
+    className?: string;
+    onClick?: () => void;
+    disabled?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({text, className, onClick, disabled = false}) => {
+    return (
+        <button className={`button ${className}`} onClick={onClick} disabled={disabled}>
+            {text}
+        </button>
+    );
+}
+
+export default Button;
