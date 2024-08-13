7a473dbf66cd700c5f248e40d4a28b442302bba3
Matt <matthieu.coisnon@decathlon.com>
2024-08-13 21:24:53 +0200

    feat(home-page): export api url into env var

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
     plugins: [
