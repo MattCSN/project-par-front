f2fb0790460393e444738f40e9bbcc88f9f1ef22
Matt <matthieu.coisnon@decathlon.com>
2024-08-13 21:24:53 +0200

    feat(home-page): export api url into env var

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: import.meta.env.VITE_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
